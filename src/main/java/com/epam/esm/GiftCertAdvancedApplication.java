8a4b205da9cdf704164bb9208e4f445f19c512d9
Bohdan Shevyrov <bshevyrov@gmail.com>
2023-07-10 11:38:27 +0300

    Added config

--- src/main/java/com/epam/esm/GiftCertAdvancedApplication.java
+++ src/main/java/com/epam/esm/GiftCertAdvancedApplication.java
@@ -1,61 +1,17 @@
 package com.epam.esm;
 
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.startup.Tomcat;
-import org.apache.catalina.webresources.TomcatURLStreamHandlerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import javax.servlet.ServletException;
-import java.io.File;
-import java.io.IOException;
-
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
+@EnableJpaRepositories( "com.epam.esm.repository")
 public class GiftCertAdvancedApplication {
 
-	public static void main(String[] args) throws LifecycleException {
-		SpringApplication.run(GiftCertAdvancedApplication.class, args);
-
-//         final ConfigurableEnvironment environment = new StandardEnvironment();
-
-			TomcatURLStreamHandlerFactory.disable();
-
-//        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
-//                MainConfiguration.class);
-//        TagService tagDAO = applicationContext.getBean(TagServiceImpl.class);
-//        tagDAO.delete(3L);
-//        applicationContext.close();
-
-			String appBase = ".";
-			Tomcat tomcat = new Tomcat();
-			tomcat.setBaseDir(createTempDir());
-			tomcat.setPort(8080);
-			tomcat.getHost().setAppBase(appBase);
-			tomcat.addWebapp("", appBase);
-			tomcat.start();
-			tomcat.getServer().await();
-		}
-
-		// based on AbstractEmbeddedServletContainerFactory
-		private static String createTempDir() {
-			try {
-				File tempDir = File.createTempFile("tomcat.", "." + 8080);
-				tempDir.delete();
-				tempDir.mkdir();
-				tempDir.deleteOnExit();
-				return tempDir.getAbsolutePath();
-			} catch (IOException ex) {
-				throw new RuntimeException(
-						"Unable to create tempDir. java.io.tmpdir is set to " + System.getProperty("java.io.tmpdir"),
-						ex
-				);
-			}
-		}
-
-
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GiftCertAdvancedApplication.class, args);
+    }
+}
 
 
--- src/main/java/com/epam/esm/ServletInitializer.java
+++ src/main/java/com/epam/esm/ServletInitializer.java
@@ -6,34 +6,28 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.core.env.ConfigurableEnvironment;
 
 import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 public class ServletInitializer extends SpringBootServletInitializer {
-    @Autowired
-    ConfigurableEnvironment conf;
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-conf.setActiveProfiles("prod");
-return application.sources(GiftCertAdvancedApplication.class);
+        return application.sources(GiftCertAdvancedApplication.class);
 
     }
-/*
-    @Override
-    public void onStartup(ServletContext container) {
-        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
-        rootContext.register(AppConfig.class);
-        container.setInitParameter("spring.profiles.active", "PROD");
-        container.addListener(new ContextLoaderListener(rootContext));
-
-        ServletRegistration.Dynamic dispatcher = container
-                .addServlet("dispatcher", new DispatcherServlet(rootContext));
-        dispatcher.setLoadOnStartup(1);
-        dispatcher.addMapping("/");
-
-}*/
 
     @Override
-    public void onStartup(ServletContext servletContext)  {
+    public void onStartup(ServletContext servletContext) {
+//        servletContext.setInitParameter("spring.profiles.active", "prod");
+//        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+//        rootContext.register(AppConfig.class);
         servletContext.setInitParameter("spring.profiles.active", "prod");
+//        container.addListener(new ContextLoaderListener(rootContext));
+//
+//        ServletRegistration.Dynamic dispatcher = servletContext
+//                .addServlet("dispatcher", new DispatcherServlet(rootContext));
+//        dispatcher.setLoadOnStartup(1);
+//        dispatcher.addMapping("/");
+
     }
 }
