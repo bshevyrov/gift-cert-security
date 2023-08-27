6e44c71bae4fdb45d20e1655dc6893fc63911ee9
Bohdan Shevyrov <bshevyrov@gmail.com>
2023-08-28 00:32:48 +0300

    Added repository layer

---
+++ src/main/java/com/epam/esm/persistence/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.epam.esm.persistence.repository;
+
+import com.epam.esm.persistence.entity.CustomerEntity;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends PagingAndSortingRepository<CustomerEntity, Long> {
+    Optional<CustomerEntity> findByUsername(String username);
+}
---
+++ src/main/java/com/epam/esm/persistence/repository/OrderItemRepository.java
@@ -0,0 +1,7 @@
+package com.epam.esm.persistence.repository;
+
+import com.epam.esm.persistence.entity.OrderItemEntity;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface OrderItemRepository extends PagingAndSortingRepository<OrderItemEntity,Long> {
+}
