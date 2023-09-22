6238eac6b19eebbb7e2a4811ad87c444f4a97197
Bohdan Shevyrov <bshevyrov@gmail.com>
2023-09-22 03:22:12 +0300

    minor update

--- src/test/java/com/epam/esm/service/impl/TagServiceImplTest.java
+++ src/test/java/com/epam/esm/service/impl/TagServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.epam.esm.service.impl;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,6 +9,7 @@ class TagServiceImplTest {
 
     @Test
     void create() {
+        Assertions.assertTrue(true);
     }
 
     @Test
