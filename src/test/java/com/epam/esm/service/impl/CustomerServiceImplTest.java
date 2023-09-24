e7d3669bf9f5b961c125e6007cfddf92eadcb9b5
Bohdan Shevyrov <bshevyrov@gmail.com>
2023-09-24 21:44:57 +0300

    Updated tests.

--- src/test/java/com/epam/esm/service/impl/CustomerServiceImplTest.java
+++ src/test/java/com/epam/esm/service/impl/CustomerServiceImplTest.java
@@ -1,54 +1,59 @@
 package com.epam.esm.service.impl;
 
 import com.epam.esm.persistence.entity.CustomerEntity;
-import com.epam.esm.persistence.repository.CustomerRepository;
-import com.epam.esm.persistence.repository.OrderRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Pageable;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+
 @ExtendWith(MockitoExtension.class)
-
 class CustomerServiceImplTest {
-    @Mock
-    private CustomerRepository customerRepository;
 
-    @InjectMocks
-    private CustomerServiceImpl customerService ;
+
+    @Mock
+    private CustomerServiceImpl customerService;
 
     @Test
     void create() {
+        assertDoesNotThrow(() -> customerService.create(new CustomerEntity()));
     }
 
     @Test
     void update() {
-        Assertions.assertThrowsExactly(UnsupportedOperationException.class,
-                ()->customerService.update(new CustomerEntity()));
+     assertDoesNotThrow(
+                () -> customerService.update(new CustomerEntity()));
     }
 
     @Test
     void findById() {
+        assertDoesNotThrow(() -> customerService.findById(anyLong()));
     }
 
     @Test
     void delete() {
-        Assertions.assertThrowsExactly(UnsupportedOperationException.class,
-                ()->customerService.delete(0L));
+        assertDoesNotThrow(
+                () -> customerService.delete(0L));
     }
 
     @Test
     void findAll() {
+        assertDoesNotThrow(() -> customerService.findAll(Pageable.unpaged()));
     }
 
     @Test
     void findByUsername() {
+        assertDoesNotThrow(() -> customerService.findByUsername(anyString()));
     }
 
     @Test
     void existsByUsername() {
+        assertDoesNotThrow(() -> customerService.existsByUsername(anyString()));
     }
 }
--- src/test/java/com/epam/esm/service/impl/GiftCertificateServiceImplTest.java
+++ src/test/java/com/epam/esm/service/impl/GiftCertificateServiceImplTest.java
@@ -1,32 +1,56 @@
 package com.epam.esm.service.impl;
 
+import com.epam.esm.persistence.entity.GiftCertificateEntity;
+import com.epam.esm.persistence.entity.TagEntity;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Pageable;
+import sun.jvm.hotspot.debugger.Page;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.lang.reflect.ParameterizedType;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.ArgumentMatchers.anyLong;
+@ExtendWith(MockitoExtension.class)
 
 class GiftCertificateServiceImplTest {
+    @Mock
+    private GiftCertificateServiceImpl giftCertificateService;
 
     @Test
     void create() {
+        assertDoesNotThrow(
+                () -> giftCertificateService.create(new GiftCertificateEntity()));
     }
 
     @Test
     void update() {
+        assertDoesNotThrow(() -> giftCertificateService.update(new GiftCertificateEntity()));
     }
 
     @Test
     void findById() {
+        assertDoesNotThrow(() -> giftCertificateService.findById(
+                anyLong()));
     }
 
     @Test
     void findAll() {
+        assertDoesNotThrow(() -> giftCertificateService.findAll(Pageable.unpaged()));
     }
 
     @Test
     void findAllByTagsName() {
+        assertDoesNotThrow(() -> giftCertificateService.findAllByTagsName(new ArrayList<>(),Pageable.unpaged()));
     }
 
     @Test
     void delete() {
+        assertDoesNotThrow(() -> giftCertificateService.delete(anyLong()));
+
     }
 }
